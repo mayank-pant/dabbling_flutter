5429b77f1ea1cbed72fce76ac9a8755c624d34ba
mayank18 <mayank@fplabs.tech>
2022-01-23 01:07:50 +0530

    httpreq and did some tinkering with card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
-import 'package:awesome_app/new_card_widget.dart';
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_typing_uninitialized_variables, prefer_const_constructors
 
-import 'package:awesome_app/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:awesome_app/drawer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:math' as math;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,9 +13,28 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+//context - study about it
 class _HomePageState extends State<HomePage> {
-  var myText = "change my name";
-  TextEditingController _nameController = TextEditingController();
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  fetchData() async {
+    var res = await http.get(Uri.parse(url));
+    data = jsonDecode(res.body);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,22 +43,39 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: CardWidget(myText: myText, nameController: _nameController),
-          ),
-        ),
-      ),
+      body: data != null
+          ? Card(
+              color: Colors.red,
+              child: GridView.builder(
+                //Listview in place of gridview can also be used but remove griddelegate
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color:
+                        Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+                            .withOpacity(1.0),
+                    child: ListTile(
+                      title: Text(data[index]["title"]),
+                      subtitle: Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: MyDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.send),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   // onPressed: () {
+      //   //   myText = _nameController.text;
+      //   //   setState(() {});
+      //   // },
+      //   child: Icon(Icons.send),
+      // ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
