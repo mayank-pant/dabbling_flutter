53ee05665432953fdd0604cc8e63b21f8ee488f3
mayank18 <mayank@fplabs.tech>
2022-01-22 22:27:51 +0530

    before refactoring - all the changes, lesson are in one file

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,271 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+//Assets | Card |Padding | SizedBox | Single child scroll View |
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "change my name";
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Awesome App"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(children: [
+                Image.asset(
+                  "assets/bg.jpg",
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter Some Text",
+                      labelText: "Name",
+                    ),
+                  ),
+                )
+              ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Mayank Pant"),
+              accountEmail: Text("pant7986@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1630710478039-9c680b99f800?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+                ),
+              ),
             ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Mayank Pant"),
+              subtitle: Text("DEV"),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("pant7986@gmail.com"),
+              subtitle: Text("Gmail"),
+              trailing: Icon(Icons.edit),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
+
+//Material Drawer | Listview | circle avatar | NetworkImage
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Awesome App"),
+//       ),
+//       body: Container(
+//         color: Colors.teal,
+//         height: 500,
+//       ),
+//       drawer: Drawer(
+//         child: ListView(
+//           padding: EdgeInsets.all(0),
+//           children: [
+//             UserAccountsDrawerHeader(
+//               accountName: Text("Mayank Pant"),
+//               accountEmail: Text("pant7986@gmail.com"),
+//               currentAccountPicture: CircleAvatar(
+//                 backgroundImage: NetworkImage(
+//                   "https://images.unsplash.com/photo-1630710478039-9c680b99f800?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+//                 ),
+//               ),
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.person),
+//               title: Text("Mayank Pant"),
+//               subtitle: Text("DEV"),
+//               trailing: Icon(Icons.edit),
+//               onTap: () {},
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.email),
+//               title: Text("pant7986@gmail.com"),
+//               subtitle: Text("Gmail"),
+//               trailing: Icon(Icons.edit),
+//             )
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: Icon(Icons.edit),
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+//     );
+//   }
+// }
+
+//Center can i have only one child
+//Row and column can have multiple child
+//Row is to place elements horizontallly
+//column places element vertically
+//row has horizontal mainaxis and vertical cross axis
+//column has vertical mainaxis and horizontal cross axis
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Awesome App"),
+//       ),
+//       body: Container(
+//         color: Colors.teal,
+//         height: 500,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Container(
+//               padding: const EdgeInsets.all(8),
+//               width: 100,
+//               height: 100,
+//               alignment: Alignment.center,
+//               color: Colors.red,
+//             ),
+//             Container(
+//               padding: const EdgeInsets.all(8),
+//               width: 100,
+//               height: 100,
+//               alignment: Alignment.center,
+//               color: Colors.yellow,
+//             ),
+//             Container(
+//               padding: const EdgeInsets.all(8),
+//               width: 100,
+//               height: 100,
+//               alignment: Alignment.center,
+//               color: Colors.green,
+//             ),
+//           ],
+//         ),
+//       ),
+//       drawer: Drawer(
+//         child: ListView(
+//           children: [DrawerHeader(child: Text("Hi i am a drawer"))],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: Icon(Icons.edit),
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+//     );
+//   }
+// }
+
+//Decoration - Most powerful widget container and Text
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Awesome App"),
+//       ),
+//       body: Center(
+//         child: Container(
+//           padding: const EdgeInsets.all(8),
+//           width: 100,
+//           height: 100,
+//           alignment: Alignment.center,
+//           decoration: BoxDecoration(
+//               color: Colors.red,
+//               borderRadius: BorderRadius.circular(10),
+//               gradient: LinearGradient(colors: [
+//                 Colors.pink,
+//                 Colors.yellow,
+//               ]),
+//               boxShadow: [
+//                 BoxShadow(
+//                   color: Colors.grey,
+//                   blurRadius: 10,
+//                 )
+//               ]),
+//           child: Text("I am a box",
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 fontSize: 20,
+//                 color: Colors.white,
+//                 fontWeight: FontWeight.bold,
+//               )),
+//         ),
+//       ),
+//     );
+//   }
+// }
